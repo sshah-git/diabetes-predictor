e12167f8905e3c29c37e1d0e2a187e18a3cb217d
sshah-git <its.sanashah@gmail.com>
2025-08-13 20:50:24 -0600

    Fix frontend folder: remove inner git repo, add as normal files

--- backend/Dockerfile
+++ backend/Dockerfile
@@ -1,5 +1,5 @@
 # Use official lightweight Python image
-FROM python:3.9-slim
+FROM python:3.11-slim
 
 # Set working directory
 WORKDIR /app
--- backend/app/app.py
+++ backend/app/app.py
@@ -28,6 +28,10 @@ class DiabetesInput(BaseModel):
     DPF: float  # Diabetes Pedigree Function
     Age: int
 
+feature_names = ['Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI', 'DPF', 'Age']
+importances = model.feature_importances_
+sorted_features = sorted(zip(feature_names, importances), key=lambda x: x[1], reverse=True)
+top_features = sorted_features[:4]
 
 @app.post("/predict")
 def predict(input_data: DiabetesInput):
@@ -36,4 +40,13 @@ def predict(input_data: DiabetesInput):
                       input_data.DPF, input_data.Age]])
     prediction = model.predict(data)
     result = "Likely to have diabetes" if prediction[0] == 1 else "Not likely to have diabetes"
-    return {"prediction": result}
+    
+    input_dict = input_data.dict()
+
+    top_features_vals = {name: input_dict[name] for name, _ in top_features}
+    resExplanation = f"Key factors: {', '.join(f'{name} ({val})' for name, val in top_features_vals.items())}"
+    
+    return {
+        "prediction": result,
+        "explanation": resExplanation
+    }
